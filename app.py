ef5607b5ab6006c5a6972ae673935f7d95ea92eb
Chris Morgan <courtneychris@me.com>
2021-06-11 10:25:22 -0400

    final flask app with all routes

--- app.py
+++ app.py
@@ -42,6 +42,7 @@ def precipitation():
    prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
    precipitation = session.query(Measurement.date, Measurement.prcp).\
     filter(Measurement.date >= prev_year).all()
+   # create a dictionary with the date as the key and the precipitation as the value and "jsonify" our dictionary
    precip = {date: prcp for date, prcp in precipitation}
    return jsonify(precip)
 
@@ -50,6 +51,39 @@ def precipitation():
 
 def stations():
     results = session.query(Station.station).all()
-    # unravel results into a one-dimensional array
+    # unravel results into a one-dimensional array (note: jsonify argument changes with list)
     stations = list(np.ravel(results))
     return jsonify(stations=stations)
+
+# build route for temperature analysis
+@app.route("/api/v1.0/tobs")
+
+def temp_monthly():
+    prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+    results = session.query(Measurement.tobs).\
+      filter(Measurement.station == 'USC00519281').\
+      filter(Measurement.date >= prev_year).all()
+    temps = list(np.ravel(results))
+    return jsonify(temps=temps)
+
+# build routes for temperature analysis (need to build for start and end dates)
+@app.route("/api/v1.0/temp/<start>")
+@app.route("/api/v1.0/temp/<start>/<end>")
+
+# start and end dates to be called in the path sent to web browser (e.g. '.../api/v1.0/temp/2017-06-01/2017-06-30'
+def stats(start=None, end=None):
+    sel = [func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)]
+
+    # if not statement to determine start and end date, run this if no end date in url path
+    if not end:
+        # asterix is used here to denote that there will be multiple results for our query
+        results = session.query(*sel).\
+            filter(Measurement.date >= start).all()
+        temps = list(np.ravel(results))
+        return jsonify(temps=temps)
+
+    results = session.query(*sel).\
+        filter(Measurement.date >= start).\
+        filter(Measurement.date <= end).all()
+    temps = list(np.ravel(results))
+    return jsonify(temps=temps)
